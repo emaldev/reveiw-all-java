3239940b350a5452d02918e006ee6950a81c004e
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-03 00:07:56 +0430

    java Class

---
+++ Class.java
@@ -0,0 +1,10 @@
+public class Class {
+    int x = 12; 
+    int y = 23; 
+    int sum = x + y; 
+    public static void main(String[] args){
+        Class myClass = new Class();
+        System.out.println(myClass.sum);
+    }
+    
+}
