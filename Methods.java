001a1a70ef31961eb70f87078e10392c96c48096
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-03 00:47:02 +0430

    java methods book abject practise

--- Methods.java
+++ Methods.java
@@ -1,39 +1,34 @@
 public class Methods {
-    int a;
-    int b;
-    public Methods(int a, int b){
-        this.a = a;
-        this.b = b;
+ 
+    String title; 
+    String author; 
+    int pages;
+  
+    
+    public  void displayInfo(){
+        System.out.println("Title: " + title + ", Author: "  + author + ", Pages: " + pages);
     }
-    public  int Sum(){
-        return a + b;
+    public void Play(){
+        System.out.println("Name of book is : " + title + ", Author is: " + author + ", and the page is: " + pages);
     }
-    public int Subtraction(){
-        return a - b;
-    }
-    public int Multiplication(){
-        return a * b;
 
-    } 
-    public double Division(){
-        
-        if(b == 0){
-          System.out.println("The seconde number is zero ");
-          return 0.0;
-        }else{
-           return (double) a / b;
-        }
-       
-    }
+    
     public static void main(String[] args){
-        Methods myMethods = new Methods(12, 32);
-        System.out.println(myMethods.Sum());
-         System.out.println("This is my mutltiplicatio : " + myMethods.Multiplication());
-         System.out.println("This is my Division: " + myMethods.Division());
-         System.out.println("Subtracion : " + myMethods.Subtraction());
+      Methods B1 = new Methods();
+      Methods B2 = new Methods();
+      B1.title = "Java ";
+      B1.author = "Mohammad ";
+      B1.pages = 613;
+      B1.displayInfo();
+      B2.title = "C++ Programming";
+      B2.author = "Emal Hasanzada";
+      B2.pages = 90123;
+      B2.Play();
+      
 
     
 
 
     }
 }
+
