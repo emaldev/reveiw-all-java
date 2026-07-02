97f182f301f329d3645e6a20f6775824bdc523ba
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-03 00:31:18 +0430

    java class Methodes return divisio by zero

--- Methods.java
+++ Methods.java
@@ -1,6 +1,10 @@
 public class Methods {
-    int a = 12; 
-    int b = 15; 
+    int a;
+    int b;
+    public Methods(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
     public  int Sum(){
         return a + b;
     }
@@ -12,16 +16,23 @@ public class Methods {
 
     } 
     public double Division(){
-        return (double) a / b;
+        
+        if(b == 0){
+          System.out.println("The seconde number is zero ");
+          return 0.0;
+        }else{
+           return (double) a / b;
+        }
+       
     }
     public static void main(String[] args){
-        Methods myMethods = new Methods();
+        Methods myMethods = new Methods(12, 32);
         System.out.println(myMethods.Sum());
          System.out.println("This is my mutltiplicatio : " + myMethods.Multiplication());
          System.out.println("This is my Division: " + myMethods.Division());
          System.out.println("Subtracion : " + myMethods.Subtraction());
 
-
+    
 
 
     }
