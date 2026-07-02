2bd1f09312d627637872c5da225e49e320e25bd0
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-02 23:38:20 +0430

    java ArrayList practise

--- Main.java
+++ Main.java
@@ -1,3 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args){
+
+       Scanner input = new Scanner(System.in);
+
+        ArrayList<String> list = new ArrayList<>();
+          
+        String NameOfList;
+        for(int i = 0; i < 5; i++){
+            System.out.println("Enter list name of" + (i + 1) + ": ");
+            NameOfList = input.nextLine();
+            list.add(NameOfList);
+        }
+        
+           
+        System.out.println("Your List of :");
+        for(String i: list){
+            System.out.println("List of \n "+ i); 
+        }
+        System.out.println("REmoved");
+        list.remove(3);
+        System.out.println("New List:");
+        for(String b : list){
+            System.out.println(b);
+        }
+
+         
+    }
     
 }
