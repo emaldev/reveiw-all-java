4f90a639508506362f7fcb40582005c5c2ccbc25
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-04 03:35:31 +0430

    java entery hashMap

--- Main.java
+++ Main.java
@@ -1,33 +1,19 @@
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        HashMap<String , Integer> name = new HashMap<>();
+        name.put("Ali", 19);
+        name.put("Sahar", 21);
+        name.put("Ahmad", 17);
 
-       Scanner input = new Scanner(System.in);
-
-        ArrayList<String> list = new ArrayList<>();
-          
-        String NameOfList;
-        for(int i = 0; i < 5; i++){
-            System.out.println("Enter list name of" + (i + 1) + ": ");
-            NameOfList = input.nextLine();
-            list.add(NameOfList);
-        }
-        
-           
-        System.out.println("Your List of :");
-        for(String i: list){
-            System.out.println("List of \n "+ i); 
-        }
-        System.out.println("REmoved");
-        list.remove(3);
-        System.out.println("New List:");
-        for(String b : list){
-            System.out.println(b);
-        }
-
-         
-    }
+      for(Map.Entry<String, Integer> entry: name.entrySet()){
+        System.out.println(entry.getKey() + ": " + entry.getValue());
+      }
     
+
+
+    }  
 }
