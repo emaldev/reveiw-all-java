3cc34be52dafe660756ec98b3011e5ba4935b853
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-05 12:58:56 +0330

    java arraylist student remove and search

--- StudentManagement/Main.java
+++ StudentManagement/Main.java
@@ -4,14 +4,40 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args){
         ArrayList<Student> Name = new ArrayList<>();
-        Name.add(new Student("9078", "Milad Azimi", 25, "Kabul"));
-        Name.add(new Student("6789", "Asef Habibi", 27, "Babakhshan"));
-        Name.add(new Student("4567", "Noria Asim", 23, "Tahran"));
-        Name.add(new Student("5634", "Anita ", 19, "Mashehad"));
+        Name.add(new Student("9078", "Milad Azimi", 25));
+        Name.add(new Student("6789", "Asef Habibi", 27));
+        Name.add(new Student("4567", "Noria Asim", 23));
+        Name.add(new Student("5634", "Anita ", 19));
+        Name.add(new Student("1234", "Alireza", 45));
 
+         System.out.println("This is the list of sudent.");
+         System.out.println(" ");
+         
+          
         for(Student i : Name){
+         
             System.out.println(i);
+            System.out.println(" ");
+           
         }
+        System.out.println("Total Student: " + Name.size());
+        System.out.println(" ");
+       System.out.println(Name.get(3));
+
+       Name.set(2, new Student("1111", "Sahar", 27));
+       Name.remove(1);
+       System.out.println(" ");
+         System.out.println("The List of Student after remove .");
+       for(Student st : Name){
+        System.out.println(st);
+       }
+       System.out.println(" ");
+       System.out.println(Name.isEmpty());
+       System.out.println(" ");
+
+    
+
+
     }
     
 }
--- StudentManagement/Student.java
+++ StudentManagement/Student.java
@@ -4,17 +4,17 @@ public class Student {
     public String id;
     public String name;
     public int age;
-    public String from;
-    public Student(String id, String name, int age, String from){
+    
+    public Student(String id, String name, int age){
         this.id = id;
         this.name = name;
         this.age = age;
-        this.from = from;
+        
     }
 
    @Override 
    public String toString(){
-    return "Id: " + id + ",Name: " + name + ", Age: " + age + ",From<Country: " + from;
+    return "Id: " + id + ",Name: " + name + ", Age: " + age;
    }
     
 }
